9074db6b76571fd74cbfbf68c643ba3cc4c14ef6
Dan Chimento <danchimento@live.com>
2021-02-16 07:06:50 -0500

    Removing bootstrap

--- growing-zone-finder/plugin.php
+++ growing-zone-finder/plugin.php
@@ -12,19 +12,16 @@
 
 function showWidget() {
 
-return "<div id='zone-finder' class='container-sm text-center p-3 mt-3'>"
-    .   "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css' rel='stylesheet'
-    integrity='ha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl' crossorigin='anonymous'>"
-    .   "<link rel='stylesheet' href='" . plugin_dir_url( __FILE__ ) . "app.css'></script>"
-    .   "<img src='" . plugin_dir_url( __FILE__ ) . "logo_half.png'></script>"
-    .   "<h1>Growing Zone Finder</h1>"
-    .   "<h6 class='text-muted mb-3'>Enter your zip code to find information about your growing zone</h6>"
-    .   "<div class='mb-3'>"
-        . "<input type='text' placeholder='Enter Your Zip Code...' id='zipcode' class='form-control text-center' />"
-    .   "</div>"
-    .   "<div class='d-flex flex-column' id='results-container' >"
-    .   "</div>"
-    .   "<script src='" . plugin_dir_url( __FILE__ ) . "app.js'></script>"
+return "<div id='zone-finder'>"
+    .       "<link rel='stylesheet' href='" . plugin_dir_url( __FILE__ ) . "app.css'></script>"
+    .       "<img src='" . plugin_dir_url( __FILE__ ) . "logo_half.png'></script>"
+    .       "<h1>Growing Zone Finder</h1>"
+    .       "<h6>Enter your zip code to find information about your growing zone</h6>"
+    .       "<div class='form-group'>"
+    .           "<input type='text' placeholder='Enter Your Zip Code...' id='zipcode' />"
+    .       "</div>"
+    .       "<div id='results-container' ></div>"
+    .       "<script src='" . plugin_dir_url( __FILE__ ) . "app.js'></script>"
     . "</div>";
 
 }
